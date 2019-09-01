3f46119fe603f01522bdf75ba7bb39fcc5feff9a
Adriana Pele <pele_adriana@yahoo.com>
2019-09-01 14:55:04 +0300

    add medicine provider

---
+++ app/src/main/java/com/example/medicinereminder/data/MedicineProvider.java
@@ -0,0 +1,53 @@
+package com.example.medicinereminder.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class MedicineProvider extends ContentProvider
+{
+    private MedicineDatabseHelper dbHelper;
+
+    @Override
+    public boolean onCreate()
+    {
+        dbHelper = new MedicineDatabseHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder)
+    {
+        SQLiteDatabase readableDatabase = dbHelper.getReadableDatabase();
+
+
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
